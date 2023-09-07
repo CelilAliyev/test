88613096fbb2e7bd8097995b579ad201c951b5a6
CelilAliyev <74897272+CelilAliyev@users.noreply.github.com>
2023-09-07 14:13:03 +0400

    Update hw.java
    hello world

--- java.js
+++ java.js
@@ -1,5 +1,14 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      LocalTime currentTime = new LocalTime();
+		  System.out.println("The current local time is: " + currentTime);
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
